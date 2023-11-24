a2a0228ec3a093233ae95a93a82a47c557d0d247
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2023-11-24 12:41:38 +0100

    feat: create client function

--- src/clients/itunesClient.tsx
+++ src/clients/itunesClient.tsx
@@ -1,6 +1,6 @@
 const baseURL = 'https://itunes.apple.com';
 
-export default {
+const itunesClient = {
   getAllPodcast: () => {
     const options = {
         limit: 100,
@@ -16,5 +16,24 @@ export default {
             return "Error consultando listado principal";
         }
     });
-  }
+  },
+  getPodcastList: (id: string) => {
+    const options = {
+      media: 'podcast',
+        entity: 'podcastEpisode',
+        limit: '20'
+    }
+    return fetch(
+      `${baseURL}/lookup?id=${id}&media=${options.media}&entity=${options.entity}&limit=${options.limit}`,
+      { method: 'GET' },
+    ).then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+            return res.json()
+        } else {
+            return "Error consultando obteniendo información del podcast";
+        }
+    });
+  },
 };
+
+export default itunesClient
