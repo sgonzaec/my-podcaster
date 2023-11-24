3114a60d46a2a5d3103ac7b4eb47711d9ac4710b
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2023-11-24 13:29:19 +0100

    feat: add loading

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,20 +1,29 @@
 import { useDetailsData } from "../../Helpers/useDetails";
 import EpisodiesList from "../../components/EpisodiesList/EpisodiesList";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import OwnerInfo from "../../components/OwnerInfo/OwnerInfo";
-import "./List.scss"
+import "./List.scss";
 
 const List: React.FC = () => {
-  const { detail } = useDetailsData();
+  const { loading, detail } = useDetailsData();
 
   return (
-    <section className="podcast_episodies">
-        <OwnerInfo author={detail?.results?.[0]} />
-        <div className="podcast_episodies_list">
-            <p className="podcast_episodies_list_count">Episodios: {detail?.resultCount}</p>
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <section className="podcast_episodies">
+          <OwnerInfo author={detail?.results?.[0]} />
+          <div className="podcast_episodies_list">
+            <p className="podcast_episodies_list_count">
+              Episodios: {detail?.resultCount}
+            </p>
             <EpisodiesList episodies={detail?.results} />
-        </div>
-    </section>
-  )
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default List;
